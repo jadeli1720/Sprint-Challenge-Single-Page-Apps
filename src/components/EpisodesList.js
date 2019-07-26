f91f0b2bbbf1410c4c67e6b148ae72ce2dbd1fdb
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-26 11:33:10 -0600

    Added EpisodesList.js and used LocationList.js for template. Added nav tab and route in proper files

---
+++ src/components/EpisodesList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import LocationCard from './LocationCard';
+
+//https://rickandmortyapi.com/api/locations/
+export default function LocationsList() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        Axios
+            .get(`https://rickandmortyapi.com/api/episodes/`)
+            .then(res =>{
+                setData(res.data.results)
+                console.log('response',res)
+            })
+            .catch(err => {
+                console.log('error', err);
+              })
+    },[])
+
+    return <section className='character-list grid-view'>
+        {/* <LocationCard/> */}
+        {data.map((data, id) => (
+            <LocationCard
+            // key={id}
+            // name={data.name}
+            // type={data.type}
+            // dimension={data.dimension}
+            // residents={data.residents.length}
+            />
+        ))}
+
+        {console.log('Location',data)}
+    </section>
+    
+}
