7c00026daa9df2c9f6d91a1857dd37282fd31968
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-26 10:45:13 -0600

    Modified LocationList.js. Set useState() and useEffect to request the data. Console logged results and can see the array of info I need for this part

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
+//https://rickandmortyapi.com/api/locations/
 export default function LocationsList() {
+    const [data, setData] = useState();
 
+    useEffect(() => {
+        Axios
+            .get(`https://rickandmortyapi.com/api/location/`)
+            .then(res =>{
+                setData(res.data.results)
+                console.log('respons',res)
+            })
+            .catch(err => {
+                console.log('error', err);
+              })
+    },[])
+
+    return <section>
+
+        {console.log('Location',data)}
+    </section>
+    
 }
